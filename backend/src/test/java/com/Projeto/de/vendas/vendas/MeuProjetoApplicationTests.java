ab30ca42f89681031a3b60dc4c1a62421e1b545a
cleber <clebergeraldodasilva777@gmail.com>
2021-09-07 22:34:54 -0300

    Projeto de vendas

---
+++ backend/src/test/java/com/Projeto/de/vendas/vendas/MeuProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projeto.de.vendas.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
