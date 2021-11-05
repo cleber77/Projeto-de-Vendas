9a484b0992d43171cffb656a81417cead780cbc1
cleber <clebergeraldodasilva777@gmail.com>
2021-11-05 17:00:58 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-
+import Footer from "components/Footer";
+import Navbar from"components/Navbar";
+import DataTable from "components/DataTable"
 function App() {
-  return (
-    <div > 
-    <h1 className= "text-primary">Deus e muito bom  thank</h1>
-    
-    </div>
+        return (
+          <>
+            <Navbar/>
+          <div className ="container" > 
+          <h1 className= "text-primary">Deus e muito bom  thank</h1>
+          <DataTable/>
+          </div>
+        <Footer/>  
+    </>
   );
 }
 
