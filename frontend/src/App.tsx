ae26db268cd1323b3abaa47909e15047faae0606
cleber <clebergeraldodasilva777@gmail.com>
2021-11-05 19:05:08 -0300

    nov3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
 import Footer from "components/Footer";
 import Navbar from"components/Navbar";
-import DataTable from "components/DataTable"
+import DataTable from "components/DataTable";
+import BarChart   from "components/BarChart";
 function App() {
         return (
           <>
-            <Navbar/>
-          <div className ="container" > 
-          <h1 className= "text-primary">Deus e muito bom  thank</h1>
+                    <Navbar/>
+                  <div className ="container" > 
+                   <h1 className= "text-primary py -3">Dashbord de venda</h1>
+                  
+                   <div className=" row px-3">
+                     <div className= "col-sm-6">          
+                       <h5 className="tex-center tex-secondary">taxa de sucesso (%)</h5>
+                      <BarChart/>
+                    </div>
+                    
+                     <div className= "col-sm-6">          
+                       <h5    className="tex-center tex-secondary">taxa de sucesso (%)</h5>
+                      <BarChart/>
+          
+                       </div>
+                     </div>
+
+
           <DataTable/>
           </div>
         <Footer/>  
