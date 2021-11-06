a42c274acbc39d1342026573fab61407cadad4bb
cleber <clebergeraldodasilva777@gmail.com>
2021-11-06 19:05:00 -0300

    novo4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,32 @@ import Footer from "components/Footer";
 import Navbar from"components/Navbar";
 import DataTable from "components/DataTable";
 import BarChart   from "components/BarChart";
+import DonutChart from   "components/DonutChart";
 function App() {
         return (
           <>
                     <Navbar/>
                   <div className ="container" > 
-                   <h1 className= "text-primary py -3">Dashbord de venda</h1>
+                   <h1 className= "text-primary py -3">Dashbord de Vendas</h1>
                   
                    <div className=" row px-3">
                      <div className= "col-sm-6">          
-                       <h5 className="tex-center tex-secondary">taxa de sucesso (%)</h5>
+                       <h5 className="tex-center tex-secondary">Taxa de Sucesso (%)</h5>
                       <BarChart/>
                     </div>
                     
                      <div className= "col-sm-6">          
-                       <h5    className="tex-center tex-secondary">taxa de sucesso (%)</h5>
-                      <BarChart/>
+                       <h5 className="tex-center tex-secondary">Tudas a venda </h5>
+                      <DonutChart/>
           
                        </div>
                      </div>
-
-
+                      <div className="py-3">
+                        <h2 className="text-primary">Todas  Venda </h2>
+                      
+                      </div>
+                        
+              
           <DataTable/>
           </div>
         <Footer/>  
